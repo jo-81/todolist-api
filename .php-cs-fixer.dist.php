752792cf101a1e793e523ae719ea52cd6052fa2f
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2025-02-12 20:40:04 +0100

    user(#38) : create User entity

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,23 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'strict_param' => true,
+        'blank_line_before_statement' => true,
+        'ordered_imports' => [
+            'sort_algorithm' => 'length',
+            'imports_order' => ['class', 'function', 'const'],
+        ],
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder)
+;
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,22 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Factory\UserFactory;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        UserFactory::createOne([
+            'email' => 'admin@domaine.fr',
+            'plainPassword' => '0000',
+            'roles' => ['ROLE_ADMIN'],
+            'username' => 'admin',
+        ]);
+
+        // UserFactory::createMany(10);
 
         $manager->flush();
     }
