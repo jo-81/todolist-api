1dfeb7f2c08fc301e9ba420cd50290b07e7bb72d
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2025-02-13 18:17:57 +0100

    edit-profil(#6): add feature for edit user profil

--- tests/Profil/ProfilTest.php
+++ tests/Profil/ProfilTest.php
@@ -10,14 +10,35 @@ class ProfilTest extends ApiTestCase
 {
     use LoginTrait;
 
-    public function testAccessRouteMeWithUserNotLogged(): void
+    /**
+     * testAccessRouteMeWithUserNotLogged.
+     *
+     * @dataProvider getDataProviderRoutes
+     */
+    public function testAccessRouteMeWithUserNotLogged(string $method): void
     {
         $client = self::createClient();
-        $client->request('GET', '/api/me');
+        $client->request($method, '/api/me');
 
         $this->assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
     }
 
+    /**
+     * getDataProviderRoutes.
+     *
+     * @return array<array<string>>
+     */
+    public static function getDataProviderRoutes(): array
+    {
+        return [
+            ['GET'],
+            ['PUT'],
+        ];
+    }
+
+    /**
+     * testAccessRouteMeWithUserLogged.
+     */
     public function testAccessRouteMeWithUserLogged(): void
     {
         $client = self::createClient();
@@ -27,6 +48,9 @@ class ProfilTest extends ApiTestCase
         $this->assertResponseIsSuccessful();
     }
 
+    /**
+     * testReadProperties.
+     */
     public function testReadProperties(): void
     {
         $client = self::createClient();
@@ -37,5 +61,22 @@ class ProfilTest extends ApiTestCase
         $json = $response->toArray();
 
         $this->assertArrayNotHasKey('password', $json);
+    }
+
+    public function testEditProfil(): void
+    {
+        $client = self::createClient();
+        $token = $this->getToken($client, 'admin', '0000');
+
+        $client->request('PUT', '/api/me', [
+            'auth_bearer' => $token,
+            'headers' => ['Content-Type' => 'application/ld+json'],
+            'json' => [
+                'email' => 'email@domaine.fr',
+                'username' => 'email',
+            ],
+        ]);
+
+        $this->assertResponseIsSuccessful();
     }
 }
